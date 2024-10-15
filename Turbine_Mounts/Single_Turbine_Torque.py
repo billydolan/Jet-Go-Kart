4997d633ae2e00de5eab0fa52ec0c3be9c049d5e
Billy <83192688+billydolan@users.noreply.github.com>
2024-10-14 23:28:08 -0400

    Updated files

---
+++ Turbine_Mounts/Single_Turbine_Torque.py
@@ -0,0 +1,10 @@
+thrust_m = 300 
+thrust_I = 67 # Verify this is the correct number for lbf
+
+mount_to_mount_dist = .178 # metric distance between the three mounting holes on each side
+mount_height = .065 # m 
+mount_width = .048 # m
+mount_length = .1 # m 
+mount_bolt_holes = .004 # diameter, m
+
+flow_exit_angle = 20 # replace with an array between 0 and 20 degrees, can add citation
